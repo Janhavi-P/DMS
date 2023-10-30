e109edc25d162e1ad5d262af9cf62989a249276c
jn1111 <janhavinalwadey1@gmail.com>
2023-10-30 13:36:31 +0530

    upload and download ui

--- src/angular_UI/demo/src/app/app.component.ts
+++ src/angular_UI/demo/src/app/app.component.ts
@@ -1,10 +1,61 @@
+import { HttpClient, HttpEventType } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'demo';
+  
+  constructor(private httpClient: HttpClient) { }
+  selectedFile: any;
+  retrievedImage: any;
+  base64Data: any;
+  retrieveResonse: any;
+  message!: string;
+  imageName: any;
+  //Gets called when the user selects an image
+  
+  //Gets called when the user clicks on submit to upload the image
+  public onFileChanged(event: Event) {
+    const target = event.target as HTMLInputElement | null;
+    
+    if (target) {
+      this.selectedFile = target.files?.[0];
+    }
+  }
+  
+  onUpload() {
+    console.log(this.selectedFile);
+  
+    // FormData API provides methods and properties to prepare form data to be sent with POST HTTP requests.
+    const uploadImageData = new FormData();
+    uploadImageData.append('files', this.selectedFile, this.selectedFile.name);
+  
+    console.log(uploadImageData);
+    // Make a call to the Spring Boot Application to save the image using the FormData object
+    this.httpClient.post('http://localhost:8080/file/upload', uploadImageData, { observe: 'response' })
+      .subscribe((response) => {
+        if (response.status === 200) {
+          this.message = 'Documemt uploaded successfully';
+        } else {
+          this.message = 'Document not uploaded successfully';
+        }
+      });
+  }
+  
+    //Gets called when the user clicks on retieve image button to get the image from back end
+    getImage() {
+    //Make a call to Sprinf Boot to get the Image Bytes.
+    this.httpClient.get('http://localhost:8080/file/download/' + this.imageName)
+      .subscribe(
+        res => {
+          this.retrieveResonse = res;
+          this.base64Data = this.retrieveResonse.picByte;
+          this.retrievedImage = 'data:image/jpeg;base64,' + this.base64Data;
+        }
+      );
+  }
 }
--- src/angular_UI/demo/src/app/app.module.ts
+++ src/angular_UI/demo/src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
